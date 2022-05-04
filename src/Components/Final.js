72a7baf1ee2d3fbd56f346717cb875ac7d5e276b
karthik singh <31167294+karthik-36@users.noreply.github.com>
2022-05-04 00:50:52 -0500

    link

--- src/Components/Final.js
+++ src/Components/Final.js
@@ -15,10 +15,12 @@ export default class About extends Component {
 
         return (
             <div>
-                <h2> Final</h2>
-                <button class="button button1"><span href="https://drive.google.com/file/d/1rFkqY6a6OyLuUzywRs7hXhzwCkBJiuwh/view" download>Download the app </span></button>
+                <h2> Final Implementation</h2>
+                <button class="button button1"><a href="https://drive.google.com/file/d/1rFkqY6a6OyLuUzywRs7hXhzwCkBJiuwh/view?usp=sharing"> Download the app </a></button>
 
                 <br />
+
+               {/* <a href="https://drive.google.com/file/d/1rFkqY6a6OyLuUzywRs7hXhzwCkBJiuwh/view?usp=sharing">download link</a> */}
 
 
 
