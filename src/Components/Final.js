28b72aa53d6d67ff34f7c842d63eb41567d22c00
karthik singh <31167294+karthik-36@users.noreply.github.com>
2022-05-03 22:37:33 -0500

    left

--- src/Components/Final.js
+++ src/Components/Final.js
@@ -23,13 +23,13 @@ export default class About extends Component {
 
 
                 <h3> Installation Instructions</h3>
-                <ul style = {{ textAlign : "center"}}>
+                <ul style={{ textAlign: "left" }}>
                     <li>follow the video instructions to install apk. <br />
                         <iframe width="560" height="315" src="https://www.youtube.com/embed/vK_jgnEe_8w" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                     </li>
 
                     <li>
-                        <ul style = {{textAlign : "left"}}>
+                        <ul style={{ textAlign: "left" }}>
                             <li>after the app is installed you need to open settings</li>
                             <li>select permissions then you need to manually give location, camera, storage permissions</li>
                         </ul>
@@ -43,15 +43,25 @@ export default class About extends Component {
 
                 </ul>
                 <br />
-
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/sYOEJpDsblU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <br />
-
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/4OiTdAj6q3E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <br />
-
-                <iframe width="560" height="315" src="https://www.youtube.com/embed/fdNZ00pELvI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
                 <br />
+                <ul style={{textAlign : "left"}}>
+
+                    <li>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/sYOEJpDsblU" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <br />
+                    </li>
+                    <li>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/4OiTdAj6q3E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <br />
+                    </li>
+
+                    <li></li>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/fdNZ00pELvI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    <br />
+
+                </ul>
             </div>
         )
     }
